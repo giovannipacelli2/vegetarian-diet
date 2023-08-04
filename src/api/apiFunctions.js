4b4a3072f32cd75f63fb4bd63c6527b2a96d1f3a
giovannipacelli2 <giovannipacelli2@gmail.com>
2023-08-04 18:04:31 +0200

    Creazione progetto

---
+++ src/api/apiFunctions.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const ACCESS_TOKEN = process.env.REACT_APP_ACCESS_TOKEN;
+
+export const fetchData = (url, params={}) =>{
+
+    const config = {
+        method: 'GET',
+        headers: {
+            "Content-type": "application/json",
+            "x-api-key": ACCESS_TOKEN
+        },
+        params
+
+    }
+
+    return axios.get(url, config);
+};
