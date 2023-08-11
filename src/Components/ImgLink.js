ea7c5e81368590f7f0a1324c3a91689955793355
giovannipacelli2 <giovannipacelli2@gmail.com>
2023-08-11 20:21:13 +0200

    Css responsive

--- src/Components/ImgLink.js
+++ src/Components/ImgLink.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 import './css/ImgLink.css';
 
 const ImgLink = ({title, image, id}) => {
+
+  const navigate = useNavigate();
+
+  const goTo = ()=> {
+    navigate(`/SingleRecipe/${id}`)
+  };
+
   return (
     <div className='img-link'>
-        <img src={image} alt={title} />
+        <img src={image} alt={title} onClick={goTo}/>
         <button type='button' className='btn btn-green img-btn'>
             <Link 
                 className='none-link' 
